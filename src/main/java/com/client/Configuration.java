9053e23ee5fbf69e85649a7d3e6b520f0bfe6975
Inno2019 <cyniclescarce@gmail.com>
2019-12-10 13:06:19 +0000

    push to get latest client from 10th

--- src/main/java/com/client/Configuration.java
+++ src/main/java/com/client/Configuration.java
@@ -31,7 +31,7 @@ public class Configuration {
     public static int fogColor = 0xDCDBDF;
 	public static long fogDelay = 500;
 	public static final String CLIENT_TITLE = "Sovark";
-	public static final int CLIENT_VERSION = 93;//updateversion
+	public static final int CLIENT_VERSION = 95;//updateversion
 	public static Boolean LIVE_SERVER = true;
 	public static final int PORT = 43594;
 
